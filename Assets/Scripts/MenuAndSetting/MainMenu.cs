b0ad833e1a41048defa15aa50587fe99096b11ec
Lannnnh <15997426361@sina.cn>
2021-09-30 18:33:26 +0800

    主界面功能实现

--- Assets/Scripts/MenuAndSetting/MainMenu.cs
+++ Assets/Scripts/MenuAndSetting/MainMenu.cs
@@ -8,18 +8,17 @@ using UnityEngine.UI;
 public class MainMenu : MonoBehaviour
 {
     public Button mainSetting, endButton;
-    public Slider slider, mainSlider;
+    public Slider mainSlider;
     public AudioSource backgroundSound;
-    public GameObject panel, mainPanel;
+    public GameObject mainPanel;
     public ProgressMgrPanel progressMgrPanel;
-    public Sprite myFirstSprite;
+    public Sprite SettingOn, SettingOff;
     private bool isOpen = false;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        panel.GetComponent<RectTransform>().localScale = new Vector2(0, 0f);
         mainPanel.GetComponent<RectTransform>().localScale = new Vector2(0, 0f);
     }
 
@@ -40,53 +39,22 @@ public class MainMenu : MonoBehaviour
         SceneManager.LoadScene("VideoScene");
     }
 
-    public void Volume() {
-        backgroundSound.volume = slider.value;
-    }
-
     public void MainVolume() {
         backgroundSound.volume = mainSlider.value;
     }
 
     public void OpenSetting() {
-        panel.GetComponent<RectTransform>().localScale = new Vector2(1, 1f);
-    }
-
-    public void CloseSetting() {
-        panel.GetComponent<RectTransform>().localScale = new Vector2(0, 0f);
-    }
-
-    public void LoadProgress()
-    {
-        progressMgrPanel.OpenProgressMgrPanel(FileOperationType.Load);
-    }
-
-    public void SaveProgress()
-    {
-        progressMgrPanel.OpenProgressMgrPanel(FileOperationType.Save);
-    }
-
-    public void BackToMenu()
-    {
-        // 加载场景
-        SceneManager.LoadScene("beginScene");
-    }
-
-    public void SettingVolume()
-    {
-        // isOpen: true 关闭音量设置 false 打开音量设置
+        print("open or close setting");
+        // isOpen: true 关闭设置 false 打开设置
         if (isOpen)
-        { 
+        {
             mainPanel.GetComponent<RectTransform>().localScale = new Vector2(0, 0f);
-            endButton.GetComponent<RectTransform>().anchoredPosition = new Vector2(0, -133f);
-            mainSetting.GetComponent<Image> ().sprite = myFirstSprite;
+            mainSetting.GetComponent<Image> ().sprite = SettingOff;
         } else {
             mainPanel.GetComponent<RectTransform>().localScale = new Vector2(1, 1f);
-            endButton.GetComponent<RectTransform>().anchoredPosition = new Vector2(0, -152f);
-            mainSetting.GetComponent<Image> ().sprite = Resources.Load <Sprite>("右上角-设置-on 128-64");;  
+            mainSetting.GetComponent<Image> ().sprite = SettingOn;  
         }
 
         isOpen = !isOpen;
     }
-
 }
---
+++ Assets/Scripts/MenuAndSetting/VideoAndLoad.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+using UnityEngine.UI;
+
+public class VideoAndLoad : MonoBehaviour
+{
+    public VideoPlayer videoPlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        videoPlayer.playOnAwake = false;
+        videoPlayer.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print("frame count: "+videoPlayer.frame+", frame allCount: "+videoPlayer.frameCount);
+        if (videoPlayer.frame+10 >= (long) videoPlayer.frameCount) {
+            SceneManager.LoadScene("teach");
+        }
+    }
+}
