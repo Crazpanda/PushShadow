56d52cc89f8c24d3f6019724a766d13b18b47eac
zhenjama <zhenjama@tencent.com>
2021-10-07 16:46:25 +0800

    tempFIx

--- Assets/Scripts/CameraClearFlagsImg.cs
+++ Assets/Scripts/CameraClearFlagsImg.cs
@@ -7,19 +7,17 @@ public class CameraClearFlagsImg : MonoBehaviour
 {
     public Texture imgBackground;
     public Canvas canvas;
+    public Camera camera2D;
 
     public Vector2 texScale = Vector2.one;
     public Vector2 texOffset = Vector2.zero;
-    Camera cameraComp;
     void Start()
     {
-        cameraComp = GetComponent<Camera>();
+        camera2D = GetComponent<Camera>();
     }
 
     private void OnPreRender()
-    {
-        Texture2D overlay = new Texture2D((int)cameraComp.pixelRect.width, (int)cameraComp.pixelRect.height, TextureFormat.RGB24, true);
-        //overlay.ReadPixels()
-        Graphics.Blit(imgBackground, cameraComp.targetTexture, texScale, texOffset);
+    {        
+        Graphics.Blit(imgBackground, camera2D.targetTexture, texScale, texOffset);
     }
 }
